2fc68b0313ebf923f8053eb00a4d32948816c781
sergio-hernandezc <sergioyeyo2003@gmail.com>
2025-04-18 15:14:25 -0500

    fixed cors

--- app/main.py
+++ app/main.py
@@ -9,7 +9,8 @@ from app.api.endpoints import auth, crypto, user_files # key_exchange is within 
 origins = [
     "http://localhost:3000",  # Default Next.js dev port
     "http://127.0.0.1:3000",
-    "https://encryptdecrypt-frontend.vercel.app/home",
+    "https://encryptdecrypt-frontend.vercel.app",
+    "https://encryptdecrypt-frontend-git-main-sergio-hernandezcs-projects.vercel.app",
     # Add your deployed frontend URL here for production
 ]
 
