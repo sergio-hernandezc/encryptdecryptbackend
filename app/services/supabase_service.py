27bad16ad5a78148fa5161b8865f53a6e98323a4
sergio-hernandezc <sergioyeyo2003@gmail.com>
2025-04-18 18:24:40 -0500

    fixed cors

--- app/services/supabase_service.py
+++ app/services/supabase_service.py
@@ -3,7 +3,7 @@ from supabase import create_client
 
 supabase = create_client(
     os.getenv("SUPABASE_URL"),
-    os.getenv("SUPABASE_SERVICE_ROLE_KEY"),
+    os.getenv("SUPABASE_SERVICE_KEY"),
 )
 
 
